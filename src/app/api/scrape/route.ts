69cf128309433c7b4914ad85c3e30854d007f4e5
doyourbest <creative555.dev@gmail.com>
2024-10-28 12:43:33 -0500

    [update] update fetch logic

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import * as cheerio from "cheerio";
 import playwright from "playwright";
+import { connectDB } from "@/lib/mongodb";
+import { Profile } from "@/models/Profile";
 
 export async function POST(request: Request) {
   const { url } = await request.json();
@@ -20,6 +22,8 @@ export async function POST(request: Request) {
   }
 
   try {
+    await connectDB();
+
     let browser;
     if (!browser) {
       browser = await playwright.chromium.launch();
@@ -55,136 +59,144 @@ export async function POST(request: Request) {
       },
     ]);
 
-    await page.goto(url, {
-      waitUntil: "domcontentloaded",
-      timeout: 30000,
-    });
-    await page.waitForSelector(".css-139x40p");
+    while (1) {
+      await page.goto(url, {
+        waitUntil: "domcontentloaded",
+        timeout: 30000,
+      });
+      await page.waitForSelector(".css-139x40p");
 
-    const content = await page.content();
-    const $ = cheerio.load(content);
+      const content = await page.content();
+      const $ = cheerio.load(content);
 
-    const mainContent = $(".css-139x40p");
-    // console.log("mainContent: ", mainContent);
+      const mainContent = $(".css-139x40p");
+      // console.log("mainContent: ", mainContent);
 
-    const profile = {
-      userId: page.url().split("/").pop(),
-      name: mainContent.find(".css-1s8r69b").text().trim(),
-      location: mainContent.find('[title="Location"]').text().trim(),
-      age: parseInt(
-        mainContent.find('[title="Age"]').text().replace(/\D/g, "")
-      ),
-      lastSeen: mainContent
-        .find('[title="Last seen on co-founder matching"]')
-        .text()
-        .replace("Last seen ", "")
-        .trim(),
-      avatar: mainContent.find(".css-1bm26bw").attr("src"),
-      sumary: mainContent.find(".css-cyoc3t").text().trim(),
-      intro: mainContent
-        .find('span.css-19yrmx8:contains("Intro")')
-        .next(".css-1tp1ukf")
-        .text()
-        .trim(),
-      lifeStory: mainContent
-        .find('span.css-19yrmx8:contains("Life Story")')
-        .next(".css-1tp1ukf")
-        .text()
-        .trim(),
-      freeTime: mainContent
-        .find('span.css-19yrmx8:contains("Free Time")')
-        .next(".css-1tp1ukf")
-        .text()
-        .trim(),
-      other: mainContent
-        .find('span.css-19yrmx8:contains("Other")')
-        .next(".css-1tp1ukf")
-        .text()
-        .trim(),
+      const age = mainContent.find('[title="Age"]').text().replace(/\D/g, "");
 
-      accomplishments: mainContent
-        .find('span.css-19yrmx8:contains("Impressive accomplishment")')
-        .next(".css-1tp1ukf")
-        .text()
-        .trim(),
-
-      education: mainContent
-        .find('.css-19yrmx8:contains("Education")')
-        .next(".css-1tp1ukf")
-        .find(".css-kaq1dv")
-        .map((_, el) => $(el).text().trim())
-        .get(),
-
-      employment: mainContent
-        .find('.css-19yrmx8:contains("Employment")')
-        .next(".css-1tp1ukf")
-        .find(".css-kaq1dv")
-        .map((_, el) => $(el).text().trim())
-        .get(),
-
-      startup: {
-        name:
-          mainContent.find(".css-bcaew0 b").first().text().trim() !== ""
-            ? mainContent.find(".css-bcaew0 b").first().text().trim()
-            : "Potential Idea",
-        description:
-          mainContent.find(".css-bcaew0 b").first().text().trim() !== ""
-            ? mainContent
-                .find(
-                  `span.css-19yrmx8:contains("${mainContent
-                    .find(".css-bcaew0 b")
-                    .first()
-                    .text()
-                    .trim()}")`
-                )
-                .next(".css-1tp1ukf")
-                .text()
-                .trim()
-            : mainContent.find("div.css-1hla380").text().trim(),
-        progress: mainContent
-          .find('span.css-19yrmx8:contains("Progress")')
+      const profile = {
+        userId: page.url().split("/").pop(),
+        name: mainContent.find(".css-1s8r69b").text().trim(),
+        location: mainContent.find('[title="Location"]').text().trim(),
+        age: age ? parseInt(age) : null,
+        lastSeen: mainContent
+          .find('[title="Last seen on co-founder matching"]')
+          .text()
+          .replace("Last seen ", "")
+          .trim(),
+        avatar: mainContent.find(".css-1bm26bw").attr("src"),
+        sumary: mainContent.find(".css-cyoc3t").text().trim(),
+        intro: mainContent
+          .find('span.css-19yrmx8:contains("Intro")')
           .next(".css-1tp1ukf")
           .text()
           .trim(),
-        funding: mainContent
-          .find('span.css-19yrmx8:contains("Funding Status")')
+        lifeStory: mainContent
+          .find('span.css-19yrmx8:contains("Life Story")')
           .next(".css-1tp1ukf")
           .text()
           .trim(),
-      },
+        freeTime: mainContent
+          .find('span.css-19yrmx8:contains("Free Time")')
+          .next(".css-1tp1ukf")
+          .text()
+          .trim(),
+        other: mainContent
+          .find('span.css-19yrmx8:contains("Other")')
+          .next(".css-1tp1ukf")
+          .text()
+          .trim(),
 
-      cofounderPreferences: {
-        requirements: mainContent
-          .find(".css-1hla380 p")
+        accomplishments: mainContent
+          .find('span.css-19yrmx8:contains("Impressive accomplishment")')
+          .next(".css-1tp1ukf")
+          .text()
+          .trim(),
+
+        education: mainContent
+          .find('.css-19yrmx8:contains("Education")')
+          .next(".css-1tp1ukf")
+          .find(".css-kaq1dv")
           .map((_, el) => $(el).text().trim())
           .get(),
-        idealPersonality: mainContent
-          .find('span.css-19yrmx8:contains("Ideal co-founder")')
-          .next(".css-1tp1ukf")
-          .text()
-          .trim(),
-        equity: mainContent
-          .find('span.css-19yrmx8:contains("Equity expectations")')
-          .next(".css-1tp1ukf")
-          .text()
-          .trim(),
-      },
 
-      interests: {
-        shared: mainContent
-          .find(".css-1v9f1hn")
+        employment: mainContent
+          .find('.css-19yrmx8:contains("Employment")')
+          .next(".css-1tp1ukf")
+          .find(".css-kaq1dv")
           .map((_, el) => $(el).text().trim())
           .get(),
-        personal: mainContent
-          .find(".css-1lw35t7")
-          .map((_, el) => $(el).text().trim())
-          .get(),
-      },
 
-      linkedIn: mainContent.find(".css-107cmgv").attr("title"),
-    };
+        startup: {
+          name:
+            mainContent.find(".css-bcaew0 b").first().text().trim() !== ""
+              ? mainContent.find(".css-bcaew0 b").first().text().trim()
+              : "Potential Idea",
+          description:
+            mainContent.find(".css-bcaew0 b").first().text().trim() !== ""
+              ? mainContent
+                  .find(
+                    `span.css-19yrmx8:contains("${mainContent
+                      .find(".css-bcaew0 b")
+                      .first()
+                      .text()
+                      .trim()}")`
+                  )
+                  .next(".css-1tp1ukf")
+                  .text()
+                  .trim()
+              : mainContent.find("div.css-1hla380").text().trim(),
+          progress: mainContent
+            .find('span.css-19yrmx8:contains("Progress")')
+            .next(".css-1tp1ukf")
+            .text()
+            .trim(),
+          funding: mainContent
+            .find('span.css-19yrmx8:contains("Funding Status")')
+            .next(".css-1tp1ukf")
+            .text()
+            .trim(),
+        },
 
-    return NextResponse.json(profile);
+        cofounderPreferences: {
+          requirements: mainContent
+            .find(".css-1hla380 p")
+            .map((_, el) => $(el).text().trim())
+            .get(),
+          idealPersonality: mainContent
+            .find('span.css-19yrmx8:contains("Ideal co-founder")')
+            .next(".css-1tp1ukf")
+            .text()
+            .trim(),
+          equity: mainContent
+            .find('span.css-19yrmx8:contains("Equity expectations")')
+            .next(".css-1tp1ukf")
+            .text()
+            .trim(),
+        },
+
+        interests: {
+          shared: mainContent
+            .find(".css-1v9f1hn")
+            .map((_, el) => $(el).text().trim())
+            .get(),
+          personal: mainContent
+            .find(".css-1lw35t7")
+            .map((_, el) => $(el).text().trim())
+            .get(),
+        },
+
+        linkedIn: mainContent.find(".css-107cmgv").attr("title"),
+      };
+
+      // After scraping the profile data
+      await Profile.findOneAndUpdate({ userId: profile.userId }, profile, {
+        upsert: true,
+        new: true,
+      });
+    }
+
+    return NextResponse.json({ message: "Profile scraped successfully" });
   } catch (error: unknown) {
     const errorMessage =
       error instanceof Error ? error.message : "Unknown error occurred";
