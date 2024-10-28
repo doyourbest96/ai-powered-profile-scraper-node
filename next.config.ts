f22a92bc5b38d397bda5bae19b48175216caee52
doyourbest <creative555.dev@gmail.com>
2024-10-28 01:31:15 -0500

    [update] update next config file fro image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["bookface-images.s3.amazonaws.com"],
+  },
 };
 
 export default nextConfig;
