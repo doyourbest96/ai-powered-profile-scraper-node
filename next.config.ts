87b74b13c0792740a75994ec500cb53f66e7574b
doyourbest <creative555.dev@gmail.com>
2024-10-28 12:43:17 -0500

    [fix] issus in avatar

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["bookface-images.s3.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.startupschool.org",
+      },
+      {
+        protocol: "https",
+        hostname: "bookface-images.s3.amazonaws.com",
+      },
+    ],
   },
 };
 
