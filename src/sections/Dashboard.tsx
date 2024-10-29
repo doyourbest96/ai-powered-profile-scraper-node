34906527edeff6993bc061326ea5ad7a0428e20b
doyourbest <creative555.dev@gmail.com>
2024-10-29 02:22:03 -0500

    [refactor] update dashboard using components

--- src/sections/Dashboard.tsx
+++ src/sections/Dashboard.tsx
@@ -2,8 +2,15 @@
 import React, { useCallback, useEffect } from "react";
 import { ProfileModel, FilterModel } from "@/types";
 import ProfileOverview from "@/sections/ProfileOverview";
+import ProfileFilter from "@/components/ProfileFilter/ProfileFilter";
+import ProfileTable from "@/components/ProfileTable/ProfileTable";
+import ProfileFooter from "@/components/ProfileFooter/ProfileFooter";
 
 const Dashboard = () => {
+  const [total, setTotal] = React.useState(0);
+  const [matched, setMatched] = React.useState(0);
+  const [curPage, setCurPage] = React.useState(1);
+  const [limit, setLimit] = React.useState(20);
   const [profile, setProfile] = React.useState<ProfileModel | null>(null);
   const [overview, setOverview] = React.useState<boolean>(false);
   const [loading, setLoading] = React.useState(false);
@@ -12,8 +19,6 @@ const Dashboard = () => {
     name: "",
     location: "",
     funding: "",
-    skip: "0",
-    limit: "20",
   });
 
   const fetchProfiles = useCallback(async () => {
@@ -35,18 +40,23 @@ const Dashboard = () => {
         ...filterParams,
       }).toString();
 
-      const response = await fetch(`/api/profiles?${queryParams}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
+      const response = await fetch(
+        `/api/profiles?page=${curPage}&limit=${limit}&${queryParams}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
       const data = await response.json();
       setProfiles(data.data);
+      setTotal(data.total);
+      setMatched(data.matched);
     } catch (error) {
       console.error("Error fetching profiles:", error);
     } finally {
       setLoading(false);
     }
-  }, [filter]);
+  }, [curPage, limit, filter]);
 
   useEffect(() => {
     fetchProfiles();
@@ -70,119 +80,22 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="relative p-5 w-full h-screen flex flex-col">
+      <div className="relative p-5 w-full h-screen flex flex-col gap-6">
         <ProfileOverview
           profile={profile}
           show={overview}
           handleClose={handleOverviewClose}
         />
-        <div className="bg-white p-6 rounded-lg shadow-sm">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                User Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                value={filter?.name}
-                onChange={handleFilterChange}
-                className="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                placeholder="Enter name"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="location"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Location
-              </label>
-              <input
-                type="text"
-                name="location"
-                id="location"
-                value={filter?.location}
-                onChange={handleFilterChange}
-                className="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                placeholder="Enter location"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="startup"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Funding Status
-              </label>
-              <input
-                type="text"
-                name="funding"
-                id="funding"
-                value={filter?.funding}
-                onChange={handleFilterChange}
-                className="p-2 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                placeholder="Enter funding"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="flex-1 overflow-hidden">
-          <div className="h-full overflow-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
-            <table className="min-w-full divide-y divide-gray-200 relative">
-              <thead className="text-left bg-gray-50 sticky top-0 z-10">
-                <tr>
-                  <th className="p-4 font-semibold text-gray-600 uppercase">
-                    Name
-                  </th>
-                  <th className="p-4 font-semibold text-gray-600 uppercase">
-                    Location
-                  </th>
-                  <th className="p-4 font-semibold text-gray-600 uppercase">
-                    Funding Status
-                  </th>
-                  <th className="p-4 font-semibold text-gray-600 uppercase">
-                    Last Seen
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200">
-                {loading ? (
-                  <tr>
-                    <td colSpan={5} className="text-center">
-                      Loading...
-                    </td>
-                  </tr>
-                ) : (
-                  profiles.map((profile, index) => (
-                    <tr
-                      key={profile.userId || index}
-                      className="even:bg-blue-100 hover:bg-gray-300 hover:cursor-pointer"
-                      onClick={handleOverview.bind(null, profile)}
-                    >
-                      <td className="px-2 py-2 whitespace-nowrap">
-                        {profile.name || "N/A"}
-                      </td>
-                      <td className="px-2 py-2 whitespace-nowrap">
-                        {profile.location || "N/A"}
-                      </td>
-                      <td className="px-2 py-2 max-w-80 text-ellipsis overflow-hidden whitespace-nowrap">
-                        {profile.startup?.funding || "N/A"}
-                      </td>
-                      <td className="px-2 py-2 whitespace-nowrap">
-                        {profile.lastSeen || "N/A"}
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <ProfileFilter filter={filter} handleChange={handleFilterChange} />
+        <ProfileTable loading={loading} profiles={profiles} handleOverview={handleOverview} />
+        <ProfileFooter
+          total={total}
+          matched={matched}
+          curPage={curPage}
+          limit={limit}
+          setCurPage={setCurPage}
+          setLimit={setLimit}
+        />
       </div>
     </>
   );
