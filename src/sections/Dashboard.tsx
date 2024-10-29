25c357c097cf44e2f17cabb50aac870d22680ea9
doyourbest <creative555.dev@gmail.com>
2024-10-29 01:15:56 -0500

    [feat] add profile overveiw to dashboard

--- src/sections/Dashboard.tsx
+++ src/sections/Dashboard.tsx
@@ -5,6 +5,7 @@ import ProfileOverview from "@/sections/ProfileOverview";
 
 const Dashboard = () => {
   const [profile, setProfile] = React.useState<ProfileModel | null>(null);
+  const [overview, setOverview] = React.useState<boolean>(false);
   const [loading, setLoading] = React.useState(false);
   const [profiles, setProfiles] = React.useState<ProfileModel[]>([]);
   const [filter, setFilter] = React.useState<FilterModel>({
@@ -12,7 +13,7 @@ const Dashboard = () => {
     location: "",
     funding: "",
     skip: "0",
-    limit: "100",
+    limit: "20",
   });
 
   const fetchProfiles = useCallback(async () => {
@@ -58,10 +59,23 @@ const Dashboard = () => {
     }));
   };
 
+  const handleOverview = (profile: ProfileModel) => {
+    setProfile(profile);
+    setOverview(true);
+  };
+
+  const handleOverviewClose = () => {
+    setOverview(false);
+  };
+
   return (
     <>
-      <div className="relative p-5">
-        <ProfileOverview profile={profile} />
+      <div className="relative p-5 w-full h-screen flex flex-col">
+        <ProfileOverview
+          profile={profile}
+          show={overview}
+          handleClose={handleOverviewClose}
+        />
         <div className="bg-white p-6 rounded-lg shadow-sm">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div>
@@ -117,55 +131,57 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        <div className="overflow-x-scroll">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="text-left bg-gray-50">
-              <tr>
-                <th className="p-4 font-semibold text-gray-600 uppercase">
-                  Name
-                </th>
-                <th className="p-4 font-semibold text-gray-600 uppercase">
-                  Location
-                </th>
-                <th className="p-4 font-semibold text-gray-600 uppercase">
-                  Funding Status
-                </th>
-                <th className="p-4 font-semibold text-gray-600 uppercase">
-                  Last Seen
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200">
-              {loading ? (
+        <div className="flex-1 overflow-hidden">
+          <div className="h-full overflow-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
+            <table className="min-w-full divide-y divide-gray-200 relative">
+              <thead className="text-left bg-gray-50 sticky top-0 z-10">
                 <tr>
-                  <td colSpan={5} className="text-center">
-                    Loading...
-                  </td>
+                  <th className="p-4 font-semibold text-gray-600 uppercase">
+                    Name
+                  </th>
+                  <th className="p-4 font-semibold text-gray-600 uppercase">
+                    Location
+                  </th>
+                  <th className="p-4 font-semibold text-gray-600 uppercase">
+                    Funding Status
+                  </th>
+                  <th className="p-4 font-semibold text-gray-600 uppercase">
+                    Last Seen
+                  </th>
                 </tr>
-              ) : (
-                profiles.map((profile, index) => (
-                  <tr
-                    key={profile.userId || index}
-                    className="even:bg-blue-100 hover:bg-gray-300 hover:cursor-pointer"
-                    onClick={() => setProfile(profile)}
-                  >
-                    <td className="px-2 py-1 whitespace-nowrap">
-                      {profile.name || "N/A"}
-                    </td>
-                    <td className="px-2 py-1 whitespace-nowrap">
-                      {profile.location || "N/A"}
-                    </td>
-                    <td className="px-2 py-1 max-w-80 text-ellipsis overflow-hidden whitespace-nowrap">
-                      {profile.startup?.funding || "N/A"}
-                    </td>
-                    <td className="px-2 py-1 whitespace-nowrap">
-                      {profile.lastSeen || "N/A"}
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {loading ? (
+                  <tr>
+                    <td colSpan={5} className="text-center">
+                      Loading...
                     </td>
                   </tr>
-                ))
-              )}
-            </tbody>
-          </table>
+                ) : (
+                  profiles.map((profile, index) => (
+                    <tr
+                      key={profile.userId || index}
+                      className="even:bg-blue-100 hover:bg-gray-300 hover:cursor-pointer"
+                      onClick={handleOverview.bind(null, profile)}
+                    >
+                      <td className="px-2 py-2 whitespace-nowrap">
+                        {profile.name || "N/A"}
+                      </td>
+                      <td className="px-2 py-2 whitespace-nowrap">
+                        {profile.location || "N/A"}
+                      </td>
+                      <td className="px-2 py-2 max-w-80 text-ellipsis overflow-hidden whitespace-nowrap">
+                        {profile.startup?.funding || "N/A"}
+                      </td>
+                      <td className="px-2 py-2 whitespace-nowrap">
+                        {profile.lastSeen || "N/A"}
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
