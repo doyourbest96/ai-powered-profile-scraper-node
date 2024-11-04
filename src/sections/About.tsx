a3b79e9b8d2ba7cfd9452fb83017127f457189d7
doyourbest <creative555.dev@gmail.com>
2024-11-04 10:48:41 -0500

    [update] update about component
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,9 +1,13 @@
 const About = () => {
   return (
     <>
-      <div className="flex justify-center items-center">
+      <div className="max-w-80 p-12 flex flex-col justify-center items-center gap-4">
         <h1>Profile Scraper</h1>
         <p>Powered Profile Scraper by @Victor</p>
+        <p>
+          If you have any question about this, please contact{" "}
+          <strong>@doyourbest</strong> in discord
+        </p>
       </div>
     </>
   );
