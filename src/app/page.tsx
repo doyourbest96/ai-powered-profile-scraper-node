553a81288f857db3533c3d755d27e154c85047b5
doyourbest <creative555.dev@gmail.com>
2024-11-04 04:37:54 -0500

    [update] update dashboard route
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
-import Head from "next/head";
-// import Scraper from "../components/Scraper/Scraper";
-import { ToastContainer } from "react-toastify";
 import Dashboard from "@/sections/Dashboard";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Web Scraper</title>
-        <meta name="description" content="Scrape product data easily" />
-      </Head>
-
-      <main>
-        {/* <Scraper /> */}
-        <Dashboard />
-        <ToastContainer
-          position="top-right"
-          autoClose={2500}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss={false}
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </main>
-    </div>
+    <>
+      <Dashboard />
+    </>
   );
 }
