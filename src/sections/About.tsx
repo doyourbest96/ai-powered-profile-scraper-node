48b4f22720a9bb84e9dc4e4fd4078c2d08fcc849
doyourbest <creative555.dev@gmail.com>
2024-11-04 10:31:16 -0500

    [update] update about component
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,15 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const About = () => {
   return (
-    <>
+    <div className="w-full h-full">
       <div className="max-w-80 p-12 flex flex-col justify-center items-center gap-4">
-        <h1>Profile Scraper</h1>
-        <p>Powered Profile Scraper by @Victor</p>
-        <p>
+        <h1 className="text-4xl font-semibold">Profile Scraper</h1>
+        <p className="text-xl">Powered Profile Scraper by @Victor</p>
+        <p className="text-lg">
           If you have any question about this, please contact{" "}
           <strong>@doyourbest</strong> in discord
         </p>
+        <Link href="/">
+          <Image
+            src="/cutestar.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="h-10 w-10 rounded-full"
+          />
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
